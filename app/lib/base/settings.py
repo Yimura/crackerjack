51390b62f3e0bd3ced74905836814ef25bb8546e
Yimura <24669514+Yimura@users.noreply.github.com>
2024-02-15 22:26:27 +0100

    feat(settings): add defaults to DB

--- app/lib/base/settings.py
+++ app/lib/base/settings.py
@@ -3,6 +3,27 @@ from app import db
 
 
 class SettingsManager:
+    def __init__(self) -> None:
+        self.insert_if_not_exists('hashcat_binary', '/usr/local/bin/hashcat')
+        self.insert_if_not_exists('hashcat_status_interval', 10)
+        self.insert_if_not_exists('hashcat_force', 0)
+
+        self.insert_if_not_exists('wordlists_path', '/opt/wordlists')
+        self.insert_if_not_exists('hashcat_rules_path', '/opt/rules')
+        self.insert_if_not_exists('hashcat_masks_path', '/opt')
+        
+        self.insert_if_not_exists('uploaded_hashes_path', '/tmp')
+
+    def insert_if_not_exists(self, name, value):
+        setting = ConfigModel.query.filter(ConfigModel.name == name).first()
+        if setting is None:
+            setting = ConfigModel(name=name, value=value)
+            db.session.add(setting)
+
+            db.session.commit()
+            return True
+        return False
+
     def save(self, name, value):
         setting = ConfigModel.query.filter(ConfigModel.name == name).first()
         if setting is None:
